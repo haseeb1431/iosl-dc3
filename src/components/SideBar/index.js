1ec0fa1c81e28075b5cb45db57dd659f60c8d545
Haseeb Asif <haseeb1431@gmail.com>
2019-04-25 14:33:54 +0200

    committing the initial changes

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -18,11 +18,13 @@ class SideBar extends Component {
       backgroundImage
     } = this.props;
 
+    // TODO: Logo update
     return (
       <div className="sidebar" data-color={backgroundColor} data-image={backgroundImage}>
 
         <div className="brand">
-          <a href="http://jslancer.com" className="brand-name">
+          <a href="http://dc3.com" className="brand-name">
+            
             <img src={'http://jslancer.com/wp-content/uploads/2017/04/js-lancer-logo2-1.png'} alt="logo" className="logo" />
           </a>
 
