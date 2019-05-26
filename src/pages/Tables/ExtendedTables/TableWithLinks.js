57854c94c3a4d3ebf61d4e034c9856280ace8fd8
alon <alon.cohn@oculid.com>
2019-05-26 15:33:47 +0200

    database fetch

--- src/pages/Tables/ExtendedTables/TableWithLinks.js
+++ src/pages/Tables/ExtendedTables/TableWithLinks.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 import generateData from '../generateData';
+import { Link, Redirect } from 'react-router-dom';
 
 class TableWithLinks extends Component {
-  state = {
-    items: generateData()
-  };
+  constructor(){
+    super()
+    this.state = {
+      package: {},
+      items : [],
+      isLoading: false,
+      error:null,
+      // items: generateData()
+    }
+  }
+  // state = {
+  //   items: generateData()
+  // };
+
+  componentDidMount() {
+    this.setState({ isLoading: true });   
+    fetch("http://localhost:8000/orders")
+      .then(function(response){
+        if (response.ok) {
+            return response.json();
+          } 
+          else {
+            throw new Error('Something went wrong ...');
+          }
+      })
+      .then((data) => {
+          console.log(data)
+            data.forEach(elemnt => {
+                this.state.items.push(elemnt)
+            })
+            this.setState({isLoading: false })
+            console.log(this.state.items);
+            console.log(this.state.items.length);
+
+      })
+      .catch(function(error){
+          console.log(error)
+      })
+  }
+
 
   deleteItem = itemId => {
     this.setState({
@@ -13,38 +51,45 @@ class TableWithLinks extends Component {
   }
 
   render() {
-    let { items, isShowingAlert } = this.state;
+    // let { items, isShowingAlert } = this.state;
     return (
       <div className="card">
         <div className="header">
-          <h4 className="title">Table with Links</h4>
-          <p className="category">Here is a subtitle for this table</p>
+          <h4 className="title">Packegs</h4>
+          {/* <p className="category">Here is a subtitle for this table</p> */}
         </div>
         <div className="content table-responsive table-full-width">
           <table className="table table-hover table-striped">
             <thead>
               <tr>
                 <th>ID</th>
-                <th>Name</th>
-                <th>Salary</th>
-                <th className="text-right">Salary</th>
-                <th className="text-right">Actions</th>
+                <th>Adress</th>
+                <th>Destenation</th>
+                <th className="text-right">Arrival Date</th>
+                <th className="text-middle">Details</th>
               </tr>
             </thead>
             <tbody>
-              {items.map(item => (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.name}</td>
-                  <td>{item.job}</td>
-                  <td className="text-right">$ {item.salary}</td>
-                  <td className="text-right">
-                    <a rel="tooltip"
-                      className="btn btn-info btn-simple btn-xs"
-                      data-original-title="View Profile"
-                      onClick={() => this.deleteItem(item.id)}>
-                      <i className="fa fa-remove"></i>
-                    </a>
+              {this.state.items.map(item => (
+                <tr key={item.OrderID} >
+                  <td><Link to={`/package/${item.OrderID}`} style={{color: 'blue'}}>
+                      {/* <i className="pe-7s-graph"></i> */}
+                      {item.OrderID}
+                      </Link>
+                  </td>
+                  <td>{item.PickAddressID}</td>
+                  <td>{item.DropAddressID}</td>
+                  <td className="text-right"> {item.ArrivalDate}</td>
+                  <td className="text-middle">
+                      <Link to="/package">
+                        <div className="btn btn-wd btn-info" >info</div>
+                      </Link>
+                    {/* <a rel="tooltip" */}
+                    {/* //   className="btn btn-info btn-simple btn-xs"
+                    //   // onClick={() => this.deleteItem(item.id)}
+                    //   onClick={() => <Redirect to='/package' />}>
+                    //   <i className="fa fa-remove"></i> */}
+                    {/* // </a> */}
                   </td>
                 </tr>
               ))}
