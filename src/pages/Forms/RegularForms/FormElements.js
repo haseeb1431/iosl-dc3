c75fd655e9973766b3c0f1713d83aad5b67f48c5
alon <alon.cohn@oculid.com>
2019-05-28 20:11:32 +0200

    add Form compnent

--- src/pages/Forms/RegularForms/FormElements.js
+++ src/pages/Forms/RegularForms/FormElements.js
@@ -5,22 +5,109 @@ import renderField from 'components/FormInputs/renderField';
 const FormElements = () => (
   <div className="card">
     <div className="header">
-      <h4>Form Elements</h4>
+      <h4>Register New Package</h4>
     </div>
     <div className="content">
-      <form className="form-horizontal">
+      <form onSubmit={console.log("done")} className="form-horizontal">
+
+        <legend>Pickup Address</legend>
+
         <div className="form-group">
-          <label className="control-label col-md-3">With help</label>
+          <label className="control-label col-md-3">Name</label>
           <div className="col-md-9">
             <Field
-              name="withHelp"
+              name="Sender Name"
+              placeholder = "Joh Doe"
               type="text"
-              component={renderField}
-              helpText="A block of help text that breaks onto a new line." />
+              component={renderField} 
+              helpText="as appear on your Mailbox" /> 
           </div>
         </div>
 
         <div className="form-group">
+          <label className="control-label col-md-3">country</label>
+          <div className="col-md-9">
+            <Field
+              name="withHelp"
+              type="text"
+              placeholder = "Germany"
+              component={renderField} />
+              {/* helpText="A block of help text that breaks onto a new line." /> */}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="control-label col-md-3">street</label>
+          <div className="col-md-9">
+            <Field
+              name="street"
+              placeholder = "berlinstr.88"
+              type="text"
+              component={renderField} />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="control-label col-md-3">zip</label>
+          <div className="col-md-9">
+            <Field
+              name="zip"
+              placeholder = "10235"
+              type="text"
+              component={renderField} />
+          </div>
+        </div>
+
+
+      <legend>Destenation Address</legend>
+
+        <div className="form-group">
+          <label className="control-label col-md-3">Name</label>
+          <div className="col-md-9">
+            <Field
+              name="dSender Name"
+              placeholder = "Joh Doe"
+              type="text"
+              component={renderField} 
+              helpText="as appear on reciever Mailbox" /> 
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="control-label col-md-3">country</label>
+          <div className="col-md-9">
+            <Field
+              name="dcountry"
+              type="text"
+              placeholder = "Germany"
+              component={renderField} />
+              {/* helpText="A block of help text that breaks onto a new line." /> */}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="control-label col-md-3">street</label>
+          <div className="col-md-9">
+            <Field
+              name="dstreet"
+              placeholder = "berlinstr.88"
+              type="text"
+              component={renderField} />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label className="control-label col-md-3">zip</label>
+          <div className="col-md-9">
+            <Field
+              name="dzip"
+              placeholder = "10235"
+              type="text"
+              component={renderField} />
+          </div>
+        </div>
+
+        {/* <div className="form-group">
           <label className="control-label col-md-3">Password</label>
           <div className="col-md-9">
             <Field
@@ -28,9 +115,9 @@ const FormElements = () => (
               type="password"
               component={renderField} />
           </div>
-        </div>
+        </div> */}
 
-        <div className="form-group">
+        {/* <div className="form-group">
           <label className="control-label col-md-3">Placeholder</label>
           <div className="col-md-9">
             <Field
@@ -51,24 +138,26 @@ const FormElements = () => (
               disabled={true}
               component={renderField} />
           </div>
-        </div>
+        </div> */}
+
+        <legend>Sensors</legend>
 
         <div className="form-group">
-          <label className="control-label col-md-3">Checkboxes and radios</label>
+          <label className="control-label col-md-3">Sensors</label>
           <div className="col-md-9 checkbox-group">
             <Field
               name="checkbox1"
               type="checkbox"
-              label="First Checkbox"
+              label="Heat Sensor"
               component={renderField} />
 
             <Field
               name="checkbox2"
               type="checkbox"
-              label="Second Checkbox"
+              label="Pressure Sensors"
               component={renderField} />
 
-            <Field
+            {/* <Field
               name="radioGroup"
               type="radio"
               label="Male"
@@ -80,11 +169,11 @@ const FormElements = () => (
               type="radio"
               label="Female"
               value="female"
-              component={renderField} />
+              component={renderField} /> */}
           </div>
         </div>
 
-        <div className="form-group">
+        {/* <div className="form-group">
           <label className="control-label col-md-3">Inline checkboxs</label>
           <div className="col-md-9 checkbox-group">
             <Field
@@ -103,9 +192,9 @@ const FormElements = () => (
               label="c"
               component={renderField} />
           </div>
-        </div>
+        </div> */}
 
-        <fieldset>
+        {/* <fieldset>
           <legend>Input variants</legend>
           <label className="col-sm-2 control-label">Custom Checkboxes & radios</label>
           <div className="col-sm-4 col-sm-offset-1 checkbox-group">
@@ -168,7 +257,8 @@ const FormElements = () => (
               component={renderField} />
 
           </div>
-        </fieldset>
+        </fieldset> */}
+        <button type="submit" className="btn btn-fill btn-info">Submit</button>
       </form>
     </div>
   </div>
