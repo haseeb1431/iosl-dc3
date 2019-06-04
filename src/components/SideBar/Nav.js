b8bf7c2d3d9c7bfa8c398bf1ec037778dc9e044d
Pradyumna kashyap <44785586+Pradyumnakashyap@users.noreply.github.com>
2019-06-04 19:46:29 +0200

    Navigation based on person Type

--- src/components/SideBar/Nav.js
+++ src/components/SideBar/Nav.js
@@ -7,7 +7,7 @@ import { stat } from 'fs';
 class Nav extends Component {
 
   state = {
-    persontype:1
+    persontype:2
   };
 
   render() {
@@ -24,29 +24,96 @@ class Nav extends Component {
               <p>Dashboard</p>
             </Link>
           </li>
-
+          <li className={this.isPathActive('/forms') || this.state.formMenuOpen ? 'active' : null}>
+          <a onClick={() => this.setState({ formMenuOpen: !this.state.formMenuOpen })} data-toggle="collapse">
+            <i className="pe-7s-note2"></i>
+            <p>Incident Management<b className="caret"></b></p>
+          </a>
+          <Collapse in={this.state.formMenuOpen}>
+            <div>
+              <ul className="nav">
+                <li className={this.isPathActive('/forms/validation-forms') ? 'active' : null}>
+                  <Link to="/forms/validation-forms">Create incident</Link>
+                </li>
+                <li className={this.isPathActive('/Tables/RegularTables') ? 'active' : null}>
+                  <Link to="/Tables/RegularTables">Incidents</Link>
+                </li>
+              </ul>
+            </div>
+          </Collapse>
+        </li>
         </ul>
       );
     }
     else if(this.state.persontype == 2){
       //Company
+      return (
+      <ul className="nav">
+          <li className={location.pathname === '/' ? 'active' : null}>
+            <Link to="/">
+              <i className="pe-7s-graph"></i>
+              <p>Dashboard</p>
+            </Link>
+          </li>
+
+          <li className={this.isPathActive('/components') || this.state.componentMenuOpen ? 'active' : null}>
+          <a onClick={() => this.setState({ componentMenuOpen: !this.state.componentMenuOpen })}
+            data-toggle="collapse">
+            <i className="pe-7s-plugin"></i>
+            <p>
+              User Management
+            <b className="caret"></b>
+            </p>
+          </a>
+          <Collapse in={this.state.componentMenuOpen}>
+            <div>
+              <ul className="nav">
+                  <li className={this.isPathActive('/components/buttons') ? 'active' : null}>
+                  <Link to="/components/buttons">Add Postman</Link>
+                </li>
+                <li className={this.isPathActive('/components/grid') ? 'active' : null}>
+                  <Link to="/components/grid">View Jobs </Link>
+                </li>
+                <li className={this.isPathActive('/components/icons') ? 'active' : null}>
+                  <Link to="/components/icons">Assign Package</Link>
+                </li>
+              </ul>
+            </div>
+          </Collapse>
+    </li>
+
+          <li className={this.isPathActive('/forms') || this.state.formMenuOpen ? 'active' : null}>
+          <a onClick={() => this.setState({ formMenuOpen: !this.state.formMenuOpen })} data-toggle="collapse">
+            <i className="pe-7s-note2"></i>
+            <p>Incident Management<b className="caret"></b></p>
+          </a>
+          <Collapse in={this.state.formMenuOpen}>
+            <div>
+              <ul className="nav">
+                <li className={this.isPathActive('/forms/validation-forms') ? 'active' : null}>
+                  <Link to="/forms/validation-forms">Create incident</Link>
+                </li>
+                <li className={this.isPathActive('/Tables/RegularTables') ? 'active' : null}>
+                  <Link to="/Tables/RegularTables">Incidents</Link>
+                </li>
+              </ul>
+            </div>
+          </Collapse>
+        </li>
+        </ul>
+      );
     }
     else{
       //PostMan
-    }
-
-  }
-
-                  /* --Postman-- 
-                
-                  <li className={location.pathname === '/' ? 'active' : null}>
-          <Link to="/">
-            <i className="pe-7s-graph"></i>
-            <p>Dashboard</p>
-          </Link>
-        </li>
-
-                <li className={this.isPathActive('/components') || this.state.componentMenuOpen ? 'active' : null}>
+      return (
+<ul className="nav">
+          <li className={location.pathname === '/' ? 'active' : null}>
+            <Link to="/">
+              <i className="pe-7s-graph"></i>
+              <p>Dashboard</p>
+            </Link>
+          </li>
+          <li className={this.isPathActive('/components') || this.state.componentMenuOpen ? 'active' : null}>
           <a onClick={() => this.setState({ componentMenuOpen: !this.state.componentMenuOpen })}
             data-toggle="collapse">
             <i className="pe-7s-plugin"></i>
@@ -87,62 +154,11 @@ class Nav extends Component {
             </div>
           </Collapse>
         </li>
-        
+        </ul>
+          );    }
 
-
-        --Company--
-         <li className={location.pathname === '/' ? 'active' : null}>
-          <Link to="/">
-            <i className="pe-7s-graph"></i>
-            <p>Dashboard</p>
-          </Link>
-        </li>
-                      
+  }
         
-        <li className={this.isPathActive('/forms') || this.state.formMenuOpen ? 'active' : null}>
-          <a onClick={() => this.setState({ formMenuOpen: !this.state.formMenuOpen })} data-toggle="collapse">
-            <i className="pe-7s-note2"></i>
-            <p>Incident Management<b className="caret"></b></p>
-          </a>
-          <Collapse in={this.state.formMenuOpen}>
-            <div>
-              <ul className="nav">
-                <li className={this.isPathActive('/forms/validation-forms') ? 'active' : null}>
-                  <Link to="/forms/validation-forms">Create incident</Link>
-                </li>
-                <li className={this.isPathActive('/Tables/RegularTables') ? 'active' : null}>
-                  <Link to="/Tables/RegularTables">Incidents</Link>
-                </li>
-              </ul>
-            </div>
-          </Collapse>
-        </li>
-        <li className={this.isPathActive('/components') || this.state.componentMenuOpen ? 'active' : null}>
-          <a onClick={() => this.setState({ componentMenuOpen: !this.state.componentMenuOpen })}
-            data-toggle="collapse">
-            <i className="pe-7s-plugin"></i>
-            <p>
-              User Management
-            <b className="caret"></b>
-            </p>
-          </a>
-          <Collapse in={this.state.componentMenuOpen}>
-            <div>
-              <ul className="nav">
-                  <li className={this.isPathActive('/components/buttons') ? 'active' : null}>
-                  <Link to="/components/buttons">Add Postman</Link>
-                </li>
-                <li className={this.isPathActive('/components/grid') ? 'active' : null}>
-                  <Link to="/components/grid">View Jobs </Link>
-                </li>
-                <li className={this.isPathActive('/components/icons') ? 'active' : null}>
-                  <Link to="/components/icons">Assign Package</Link>
-                </li>
-              </ul>
-            </div>
-          </Collapse>
-    </li>}
-   */
 
   isPathActive(path) {
     return this.props.location.pathname.startsWith(path);
