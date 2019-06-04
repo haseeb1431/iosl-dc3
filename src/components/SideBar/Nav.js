4a93e16c01ed1806dc7b240cf9f6f94262eb4f0d
Haseeb Asif <haseeb1431@gmail.com>
2019-06-04 19:16:24 +0200

    added conditional nav bar

--- src/components/SideBar/Nav.js
+++ src/components/SideBar/Nav.js
@@ -1,44 +1,40 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { Collapse } from 'react-bootstrap';
+import { stat } from 'fs';
 
 
 class Nav extends Component {
 
-  state = {};
+  state = {
+    persontype:1
+  };
 
   render() {
     let { location } = this.props;
-    return (
+    const isUser = this.state.persontype==1;
 
-      <ul className="nav">
-        <li className={location.pathname === '/' ? 'active' : null}>
-          <Link to="/">
-            <i className="pe-7s-graph"></i>
-            <p>Dashboard</p>
-          </Link>
-        </li>
+    if (this.state.persontype == 1) {
+      //User Navigation bar
+      return (
+        <ul className="nav">
+          <li className={location.pathname === '/' ? 'active' : null}>
+            <Link to="/">
+              <i className="pe-7s-graph"></i>
+              <p>Dashboard</p>
+            </Link>
+          </li>
 
-        <li className={this.isPathActive('/forms') || this.state.formMenuOpen ? 'active' : null}>
-          <a onClick={() => this.setState({ formMenuOpen: !this.state.formMenuOpen })} data-toggle="collapse">
-            <i className="pe-7s-note2"></i>
-            <p>Incident Management<b className="caret"></b></p>
-          </a>
-          <Collapse in={this.state.formMenuOpen}>
-            <div>
-              <ul className="nav">
-                <li className={this.isPathActive('/forms/validation-forms') ? 'active' : null}>
-                  <Link to="/forms/validation-forms">Create incident</Link>
-                </li>
-                <li className={this.isPathActive('/Tables/RegularTables') ? 'active' : null}>
-                  <Link to="/Tables/RegularTables">Incidents</Link>
-                </li>
-              </ul>
-            </div>
-          </Collapse>
-        </li>
-     </ul>
-    );
+        </ul>
+      );
+    }
+    else if(this.state.persontype == 2){
+      //Company
+    }
+    else{
+      //PostMan
+    }
+
   }
 
                   /* --Postman-- 
