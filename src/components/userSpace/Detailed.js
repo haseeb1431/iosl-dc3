416062b3520927b2a93f3e91a09414676019844f
alon <alon.cohn@oculid.com>
2019-06-24 13:37:56 +0200

    hide time line photo

--- src/components/userSpace/Detailed.js
+++ src/components/userSpace/Detailed.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tracking from 'assets/images/tracking.png';
+import RangeBar from './RangeBar'
+
 
 const EXAMPLE = [
   {
@@ -8,13 +10,14 @@ const EXAMPLE = [
     statusB: "Ready for Dev",
     statusE: "In Progress"
   },
-  /* {
+  {
     data: "2018-03-23",
     status: "status",
     statusB: "In Progress",
     statusE: "Done"
-  } */
+  }
 ];
+
 const sharp = {
   display: "inline-block", 
   fontSize: "12px",
@@ -70,11 +73,6 @@ componentDidMount() {
         console.log(error)
     })
 }
-
-    
-
-
-
 render() {
     return (
         <div>
@@ -82,46 +80,52 @@ render() {
               <div className = "row">
                 <div className = "col-md-12">
                     <div className = "col-md-6">
-                    <div className="card">
-                        <div className="header">
-                            <h4 className="title">Package Information</h4>
-                        </div>
-                        <div className = "content ">                          
-                              <div className = "control-label card"><h6 style={sharp}>Package Id: </h6><h6 style={values}>{item.OrderID }</h6></div>
-                              <div className = "control-label card"><h6 style={sharp}>Pickup Address:</h6><h6 style={values}>{item.pickstreetaddress}</h6></div>
-                              <div className = "control-label card"><h6 style={sharp}>Pickup City:</h6><h6 style={values}>{item.pickcity}</h6></div>
-                              <div className = "control-label card"><h6 style={sharp}>Pickup Country:</h6><h6 style={values}>{item.pickcountry}</h6></div>
-                              <div className = "control-label card"><h6 style={sharp}>Pickup PostCode:</h6><h6 style={values}>{item.pickpostcode}</h6></div>
-
-
-                              <div className = "control-label card"><h6 style={sharp}>Drop Address:</h6><h6 style={values}> {item.dropstreetaddress}</h6></div>
-                              <div className = "control-label card"><h6 style={sharp}>Drop City:</h6><h6 style={values}> {item.dropcity}</h6></div>
-                              <div className = "control-label card"><h6 style={sharp}>Drop Country:</h6><h6 style={values}> {item.dropcountry}</h6></div>
-                              <div className = "control-label card"><h6 style={sharp}>Drop PostCode:</h6><h6 style={values}> {item.droppostcode}</h6></div>
-
-                              <div className = "control-label card"><h6 style={sharp}>Sensor Type:</h6> <h6 style={values}>Heat Sensor </h6> </div>
-                              <div className = "control-label card"> <h6 style={sharp}> Status: </h6> <h6 style={values}>In Transit</h6></div> 
-                          
-                        </div>
-                        </div>
-                    </div>
-                  
-                    <div className = "col-md-6">
-                    <div className="card">
+                      <div className="card">
                           <div className="header">
-                              <h4 className="title">Tracking</h4>
+                              <h4 className="title">Package Information</h4>
                           </div>
-                              <div className = "col-md-12 card " > 
-                              <img src={Tracking} style={{width:"100%"}}></img>
-                              
+                          <div className = "content ">                          
+                                <div className = "control-label card"><h6 style={sharp}>Package Id: </h6><h6 style={values}>{item.OrderID }</h6></div>
+                                <div className = "control-label card"><h6 style={sharp}>Pickup Address:</h6><h6 style={values}>{item.pickstreetaddress}</h6></div>
+                                <div className = "control-label card"><h6 style={sharp}>Pickup City:</h6><h6 style={values}>{item.pickcity}</h6></div>
+                                <div className = "control-label card"><h6 style={sharp}>Pickup Country:</h6><h6 style={values}>{item.pickcountry}</h6></div>
+                                <div className = "control-label card"><h6 style={sharp}>Pickup PostCode:</h6><h6 style={values}>{item.pickpostcode}</h6></div>
 
-                              </div>
+
+                                <div className = "control-label card"><h6 style={sharp}>Drop Address:</h6><h6 style={values}> {item.dropstreetaddress}</h6></div>
+                                <div className = "control-label card"><h6 style={sharp}>Drop City:</h6><h6 style={values}> {item.dropcity}</h6></div>
+                                <div className = "control-label card"><h6 style={sharp}>Drop Country:</h6><h6 style={values}> {item.dropcountry}</h6></div>
+                                <div className = "control-label card"><h6 style={sharp}>Drop PostCode:</h6><h6 style={values}> {item.droppostcode}</h6></div>
+
+                                <div className = "control-label card"><h6 style={sharp}>Sensor Type:</h6> <h6 style={values}>Heat Sensor </h6> </div>
+                                <div className = "control-label card"> <h6 style={sharp}> Status: </h6> <h6 style={values}>In Transit</h6></div> 
+                            
+                          </div>
                       </div>
                     </div>
-                    </div>
-      
-          </div>
+                  
+                    {/* <div className = "col-md-6">
+                      <div className="card">
+                            <div className="header">
+                                <h4 className="title">Tracking</h4>
+                            </div>
+                            <div className = "col-md-12 card " > 
+                              <img src={Tracking} style={{width:"100%"}}></img>                           
+                            </div>
+                      </div>
+                    </div> */}
+                    
+
+                    
+
+              </div>     
+            </div>
             ))}
+
+            <RangeBar />
+            
+            
+            
         </div>
     )    
 }
