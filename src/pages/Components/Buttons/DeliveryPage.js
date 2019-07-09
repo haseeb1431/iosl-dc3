d49dce6352dd27a01b6c5e53ad1d42a022b4f847
haseeb <haseeb1431@gmail.com>
2019-07-09 20:27:06 +0200

    deliver functionality

--- src/pages/Components/Buttons/DeliveryPage.js
+++ src/pages/Components/Buttons/DeliveryPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-import renderField from "components/FormInputs/renderField";
+import authLib from '../../../config/authlib';
 
 class DeliveryPage extends React.Component {
   constructor(props) {
@@ -12,29 +12,68 @@ class DeliveryPage extends React.Component {
       items: [],
       deliveryData: props.match.params.dropaddressid
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     package: {}
-  //   };
-  // }
+
+  handleSubmit(values) {
+
+    values.preventDefault();
+
+    const fetchOption = authLib.getFetchOptions();
+    const userObj = authLib.getUserObj();
+
+    //TODO: Use global.backendURL
+    fetch("http://localhost:8000/package/" + this.state.deliveryData, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': fetchOption.headers['x-access-token']
+      },
+      body: JSON.stringify({
+        "Status": "Delievered"
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+
+
+    fetch("http://localhost:8000/OrderHistory", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': fetchOption.headers['x-access-token']
+      },
+      body: JSON.stringify({
+        "orderId": this.state.deliveryData,
+        "handoverDate": new Date(),
+        "status": "Delievered",
+        "postmanId": userObj.ID
+      })
+    })
+      .then(res => res.json())
+      .then(
+        (data) => {
+          console.log(data)
+        }
+      )
+  }
+
 
   render() {
-    const { handleSubmit } = this.props;
+
     return (
       <div className="card">
         <div className="header">
           <h4>Deliver Package</h4>
         </div>
-        {/* <h4 className="text-center">
-          Package ID:{this.state.deliveryData.orderid}
-        </h4> */}
         <h4 className="text-center">
           Drop Location ID:{this.state.deliveryData}
         </h4>
         <div className="content">
-          <form onSubmit={handleSubmit} className="form-horizontal">
+          <form onSubmit={this.handleSubmit} className="form-horizontal">
             <legend>The package is being delivered here</legend>
 
             <button type="submit" className="btn btn-fill btn-info">
